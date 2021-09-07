a54621a72652e0043587a5569793227ea4d8555f
Emily Cogsdill <47834372+emilycogsdill@users.noreply.github.com>
2021-09-07 10:37:17 -0500

    add parsons modules

---
+++ parsons_utilities/__init__.py
@@ -0,0 +1,9 @@
+from table import Table
+from tofrom import ToFrom
+from etl import ETL
+
+__all__ = [
+    'ETL',
+    'Table',
+    'ToFrom'
+]
---
+++ setup.py
@@ -0,0 +1,25 @@
+import setuptools
+from setuptools import setup
+import os
+
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='parsons-utilities',
+    version='0.0.0',
+    author='Emily Cogsdill',
+    author_email='emily.cogsdill@techallies.org',
+    description='Parsons modules for Airbyte deployment',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/community-tech-alliance/parsons-utilities',
+    project_urls = {
+    },
+    license='MIT',
+    packages=['parsons_utilities'],
+    install_requires=required
+)
