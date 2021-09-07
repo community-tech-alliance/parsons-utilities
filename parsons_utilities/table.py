53d8c100af90d6c6073df2acd429e06c7daeb2d2
Emily Cogsdill <47834372+emilycogsdill@users.noreply.github.com>
2021-09-07 14:21:04 -0500

    import as files

--- parsons_utilities/table.py
+++ parsons_utilities/table.py
@@ -1,6 +1,6 @@
 from parsons_utilities.etl import ETL
 from parsons_utilities.tofrom import ToFrom
-import parsons_utilities.files
+import parsons_utilities.files as files
 import petl
 import pickle
 import logging
