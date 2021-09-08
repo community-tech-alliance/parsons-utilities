289413551d33c7898bbb56f6cd717978f15b2f45
Emily Cogsdill <47834372+emilycogsdill@users.noreply.github.com>
2021-09-08 10:05:05 -0500

    adding new and improved get_events method

--- parsons_utilities/mobilize_america.py
+++ parsons_utilities/mobilize_america.py
@@ -10,7 +10,6 @@ logger = logging.getLogger(__name__)
 
 MA_URI = 'http://events.mobilizeamerica.io/api/v1/'
 
-
 class MobilizeAmerica(object):
     """
     Instantiate MobilizeAmerica Class
@@ -31,7 +30,7 @@ class MobilizeAmerica(object):
             logger.info('Mobilize America API Key missing. Calling methods that rely on private'
                         ' endpoints will fail.')
 
-    def _request(self, url, req_type='GET', post_data=None, args=None, auth=False, paginate=False):
+    def _request(self, url, req_type='GET', post_data=None, args=None, auth=False):
         if auth:
 
             if not self.api_key:
@@ -49,37 +48,17 @@ class MobilizeAmerica(object):
 
         json = r.json()['data']
 
-        # Implement pagination logic if paginate set to True
-        if paginate == True:
-            while r.json()['next']:
-                r = self._request(r.json()['next'], req_type=req_type)
-                json.extend(r.json()['data'])
+        print(f"URL: {url}")
+        print(f"NEXT: {r.json()['next']}")
+
+        while r.json()['next']:
+            url = r.json()['next']
+            r = _request(req_type, url, json=post_data, params=args, headers=header)
+            json.extend(r)
 
         return json
 
-    def _time_parse(self, time_arg):
-        # Parse the date filters
-
-        trans = [('>=', 'gte_'),
-                 ('>', 'gt_'),
-                 ('<=', 'lte_'),
-                 ('<', 'lt_')]
-
-        if time_arg:
-
-            time = re.sub('<=|<|>=|>', '', time_arg)
-            time = date_to_timestamp(time)
-            time_filter = re.search('<=|<|>=|>', time_arg).group()
-
-            for i in trans:
-                if time_filter == i[0]:
-                    return i[1] + str(time)
-
-            raise ValueError('Invalid time operator. Must be one of >=, >, <= or >.')
-
-        return time_arg
-
-    def get_organizations(self, updated_since=None):
+    def get_organizations_json(self, updated_since=None):
         """
         Return all active organizations on the platform.
 
@@ -87,18 +66,21 @@ class MobilizeAmerica(object):
             updated_since: str
                 Filter to organizations updated since given date (ISO Date)
         `Returns`
-            Parsons Table
-                See :ref:`parsons-table` for output options.
+            JSON of paginated response data.
+
+        `Sample usage`
+
+        mob = MobilizeAmerica()
+        organizations_json = mob.get_organizations_json(updated_since='2021-09-01')
         """
 
-        return Table(self._request(self.uri + 'organizations',
-                                            args={
-                                                'updated_since': date_to_timestamp(updated_since)
-                                            },
-                                   paginate=True))
+        return self._request(self.uri + 'organizations',
+                             args={
+                                 'updated_since': date_to_timestamp(updated_since)
+                             })
 
     def get_events(self, organization_id=None, updated_since=None, timeslot_start=None,
-                   timeslot_end=None, timeslots_table=False, max_timeslots=None):
+                   timeslot_end=None, timeslots_table=False, max_timeslots=None, output_format='Parsons'):
         """
         Fetch all public events on the platform.
 
@@ -130,8 +112,8 @@ class MobilizeAmerica(object):
                 running and want to ensure that the column headers remain static.
 
         `Returns`
-            Parsons Table or dict or Parsons Tables
-                See :ref:`parsons-table` for output options.
+            either Parsons Table or dict or Parsons Tables (See :ref:`parsons-table` for output options)
+            OR a JSON object (e.g. for use in Airbyte integration)
         """
 
         if isinstance(organization_id, (str, int)):
@@ -142,7 +124,9 @@ class MobilizeAmerica(object):
                 'timeslot_start': self._time_parse(timeslot_start),
                 'timeslot_end': self._time_parse(timeslot_end)}
 
-        tbl = Table(self._request_paginate(self.uri + 'events', args=args))
+        json_response = self._request(self.uri + 'events', args=args, suppress_args_on_paginate=True)
+
+        tbl = Table(json_response)
 
         if tbl.num_rows > 0:
 
@@ -153,7 +137,7 @@ class MobilizeAmerica(object):
 
             if timeslots_table:
 
-                timeslots_tbl = tbl.long_table(['id'], 'timeslots', 'event_id')
+                timeslots_tbl = tbl.long_table(['id'], 'timeslots', {'id': 'event_id'})
                 return {'events': tbl, 'timeslots': timeslots_tbl}
 
             else:
@@ -163,163 +147,25 @@ class MobilizeAmerica(object):
                     if re.search('timeslots', c, re.IGNORECASE) is not None:
                         tbl.unpack_dict(c)
 
-        return tbl
+        if output_format == 'Parsons':
 
-    def get_events_organization(self, organization_id=None, updated_since=None, timeslot_start=None,
-                                timeslot_end=None, timeslots_table=False, max_timeslots=None):
-        """
-        Fetch all public events for an organization. This includes both events owned
-        by the organization (as indicated by the organization field on the event object)
-        and events of other organizations promoted by this specified organization.
+            return tbl
 
-        .. note::
-            API Key Required
+        elif output_format == 'JSON':
+            # For the Airbyte integration, we need to output a JSON object
+            # To get from the Parsons table object to a JSON, we can save the table as a csv, then use petl to extract the csv and load it into a json, which we then return
 
-        `Args:`
-            organization_id: list or int
-                Filter events by a single or multiple organization ids
-            updated_since: str
-                Filter to events updated since given date (ISO Date)
-            timeslot_start: str
-                Filter by a timeslot start of events using ``>``,``>=``,``<``,``<=``
-                operators and ISO date (ex. ``<=2018-12-13 05:00:00PM``)
-            timeslot_end: str
-                Filter by a timeslot end of events using ``>``,``>=``,``<``,``<=``
-                operators and ISO date (ex. ``<=2018-12-13 05:00:00PM``)
-            timeslot_table: boolean
-                Return timeslots as a separate long table. Useful for extracting
-                to databases.
-            zipcode: str
-                Filter by a Events' Locations' postal code. If present, returns Events
-                sorted by distance from zipcode. If present, virtual events will not be returned.
-            max_dist: str
-                Filter Events' Locations' distance from provided zipcode.
-            visibility: str
-                Either `PUBLIC` or `PRIVATE`. Private events only return if user is authenticated;
-                if `visibility=PRIVATE` and user doesn't have permission, no events returned.
-            exclude_full: bool
-                If `exclude_full=true`, filter out full Timeslots (and Events if all of an Event's
-                Timeslots are full)
-            is_virtual: bool
-                `is_virtual=false` will return only in-person events, while `is_virtual=true` will
-                return only virtual events. If excluded, return virtual and in-person events. Note
-                that providing a zipcode also implies `is_virtual=false`.
-            event_types:enum
-                The type of the event, one of: `CANVASS`, `PHONE_BANK`, `TEXT_BANK`, `MEETING`,
-                `COMMUNITY`, `FUNDRAISER`, `MEET_GREET`, `HOUSE_PARTY`, `VOTER_REG`, `TRAINING`,
-                `FRIEND_TO_FRIEND_OUTREACH`, `DEBATE_WATCH_PARTY`, `ADVOCACY_CALL`, `OTHER`.
-                This list may expand in the future.
-            max_timeslots: int
-                If not returning a timeslot table, will unpack time slots. If do not
-                set this arg, it will add a column for each time slot. The argument
-                limits the number of columns and discards any additional timeslots
-                after that.
+            # Write table to a CSV
+            csv_filename = tbl.to_csv()
 
-                For example: If there are 20 timeslots associated with your event,
-                and you set the max time slots to 5, it will only return the first 5
-                time slots as ``time_slot_0``, ``time_slot_1`` etc.
+            # Extract the CSV into a new petl table
+            testcsv = fromcsv(csv_filename)
 
-                This is helpful in situations where you have a regular sync
-                running and want to ensure that the column headers remain static.
+            # Load the petl table into a json file
+            tojson(testcsv, 'output.json')  # convert the CSV to json
 
-        `Returns`
-            Parsons Table or dict or Parsons Tables
-                See :ref:`parsons-table` for output options.
-        """
+            # Load the json file into a JSON object
+            f = open('output.json')
+            json_output = json.load(f)
 
-        if isinstance(organization_id, (str, int)):
-            organization_id = [organization_id]
-
-        args = {'organization_id': organization_id,
-                'updated_since': date_to_timestamp(updated_since),
-                'timeslot_start': self._time_parse(timeslot_start),
-                'timeslot_end': self._time_parse(timeslot_end),
-                }
-
-        tbl = Table(self._request_paginate(self.uri + 'events', args=args, auth=True))
-
-        if tbl.num_rows > 0:
-
-            tbl.unpack_dict('sponsor')
-            tbl.unpack_dict('location', prepend=False)
-            tbl.unpack_dict('location', prepend=False)  # Intentional duplicate
-            tbl.table = petl.convert(tbl.table, 'address_lines', lambda v: ' '.join(v))
-
-            if timeslots_table:
-
-                timeslots_tbl = tbl.long_table(['id'], 'timeslots', 'event_id')
-                return {'events': tbl, 'timeslots': timeslots_tbl}
-
-            else:
-                tbl.unpack_list('timeslots', replace=True, max_columns=max_timeslots)
-                cols = tbl.columns
-                for c in cols:
-                    if re.search('timeslots', c, re.IGNORECASE) is not None:
-                        tbl.unpack_dict(c)
-
-        return tbl
-
-    def get_events_deleted(self, organization_id=None, updated_since=None):
-        """
-        Fetch deleted public events on the platform.
-
-        `Args:`
-            organization_id: list or int
-                Filter events by a single or multiple organization ids
-            updated_since: str
-                Filter to events updated since given date (ISO Date)
-        `Returns`
-            Parsons Table
-                See :ref:`parsons-table` for output options.
-        """
-
-        if isinstance(organization_id, (str, int)):
-            organization_id = [organization_id]
-
-        args = {'organization_id': organization_id,
-                'updated_since': date_to_timestamp(updated_since)}
-
-        return Table(self._request_paginate(self.uri + 'events/deleted', args=args))
-
-    def get_people(self, organization_id=None, updated_since=None):
-        """
-        Fetch all people (volunteers) who are affiliated with the organization.
-
-        .. note::
-            API Key Required
-
-        `Args:`
-            organization_id: list of int
-                Filter events by a single or multiple organization ids
-            updated_since: str
-                Filter to events updated since given date (ISO Date)
-        `Returns`
-            Parsons Table
-                See :ref:`parsons-table` for output options.
-        """
-
-        url = self.uri + 'organizations/' + str(organization_id) + '/people'
-        args = {'updated_since': date_to_timestamp(updated_since)}
-        return Table(self._request_paginate(url, args=args, auth=True))
-
-    def get_attendances(self, organization_id=None, updated_since=None):
-        """
-        Fetch all attendances which were either promoted by the organization or
-        were for events owned by the organization.
-
-        .. note::
-            API Key Required
-
-        `Args:`
-            organization_id: list of int
-                Filter events by a single or multiple organization ids
-            updated_since: str
-                Filter to events updated since given date (ISO Date)
-        `Returns`
-            Parsons Table
-                See :ref:`parsons-table` for output options.
-        """
-
-        url = self.uri + 'organizations/' + str(organization_id) + '/attendances'
-        args = {'updated_since': date_to_timestamp(updated_since)}
-        return Table(self._request_paginate(url, args=args, auth=True))
+            return json_output
